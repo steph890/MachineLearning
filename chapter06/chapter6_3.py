3ebd5fb546d47ee59455b6a336cdb458cffc03bb
sanq <steph890@naver.com>
2022-03-20 17:30:34 +0900

    220320 study

--- chapter06/chapter6_3.py
+++ chapter06/chapter6_3.py
@@ -1,3 +1,16 @@
+def draw_fruits(arr,ratio=1):
+    n = len(arr)
+    rows=int(np.ceil(n/10))
+    cols = n if rows < 2 else 10
+    fig, axs = plt.subplots(rows,cols,figsize=(cols*ratio,rows*ratio), squeeze=False)
+    for i in range(rows):
+        for j in range(cols):
+            if i*10 + j <n:
+                axs[i,j].imshow(arr[i*10+j], cmap='gray_r')
+            axs[i,j].axis('off')
+    plt.show()
+
+
 import numpy as np
 fruits=np.load('fruits_300.npy')
 fruits_2d = fruits.reshape(-1,100*100)
